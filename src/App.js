e155b42f6cb8557178553ec15c6ae11ff7eeb9c4
lwxtd <lwxtd@ymail.com>
2024-06-27 22:54:29 +0330

    initial steps

--- src/App.js
+++ src/App.js
@@ -19,9 +19,12 @@ function App() {
 
   return (
     <div className="App">
-      {fet()}
-      <header>Hello my Smartt Contract</header>
-      <div>connection to server is OK</div>
+      {/* {fet()} */}
+      <header className="text-3xl font-bold underline text-orange-400">
+        Hello my Smartt Contract
+      </header>
+      <div>connection to server is OKk</div>
+      <button className="btn bg-primary">btn</button>
     </div>
   );
 }
