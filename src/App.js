eedfb5dbd22abc008ef5c7420c51c496cf190acc
mostafa <lwxtd@ymail.com>
2024-06-27 20:01:28 +0330

    ff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const fet = () => {
+    try {
+      axios
+        .post("/users", {
+          firstName: "Fred",
+          lastName: "Flintstone",
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {fet()}
+      <header>Hello my Smartt Contract</header>
+      <div>connection to server is OK</div>
     </div>
   );
 }
